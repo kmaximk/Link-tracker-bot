df4b281f24dfe3db6bdd94d29b36ebbef8f398d6
kmaximk <entpfc@mail.ru>
2024-03-08 11:50:17 +0300

    Created migrations

--- scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
@@ -1,12 +1,34 @@
 package edu.java.scrapper;
 
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.Scope;
+import liquibase.command.CommandScope;
+import liquibase.command.core.UpdateCommandStep;
+import liquibase.command.core.helpers.DbUrlConnectionCommandStep;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.CommandExecutionException;
+import liquibase.exception.DatabaseException;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.DirectoryResourceAccessor;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterAll;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @Testcontainers
+@Slf4j
 public abstract class IntegrationTest {
     public static PostgreSQLContainer<?> POSTGRES;
 
@@ -17,11 +39,34 @@ public abstract class IntegrationTest {
             .withPassword("postgres");
         POSTGRES.start();
 
-        runMigrations(POSTGRES);
+        try {
+            runMigrations();
+        } catch (Exception e) {
+            log.error("exception in tests", e);
+        }
     }
 
-    private static void runMigrations(JdbcDatabaseContainer<?> c) {
-        // ...
+    @AfterAll
+    public static void postgresStop() {
+        POSTGRES.stop();
+    }
+
+    private static void runMigrations() throws Exception {
+        Path migrationsPath =
+            new File(".").toPath().toAbsolutePath().
+                getParent().getParent().
+                resolve("migrations");
+        java.sql.Connection connection = DriverManager.getConnection(POSTGRES.getJdbcUrl(),
+            POSTGRES.getUsername(), POSTGRES.getPassword()
+        );
+        Database database =
+            DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
+        Scope.child(Scope.Attr.resourceAccessor, new DirectoryResourceAccessor(migrationsPath), () -> {
+            CommandScope update = new CommandScope("update");
+            update.addArgumentValue("changelogFile", "master.xml");
+            update.addArgumentValue("database", database);
+            update.execute();
+        });
     }
 
     @DynamicPropertySource
